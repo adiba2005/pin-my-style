d6a113d2a1849e11195bd34e0e96cdecd079ad94
Adiba <siddiquiadiba625@gmail.com>
2024-08-22 09:40:16 +0530

    Updated login and register page, connected MongoDB (secret removed)

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const expressSession= require("express-session");
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const expressSession = require("express-session");
 const passport = require('passport');
+require('dotenv').config();
+
+// Import routers from routes directory
+var indexRouter = require('./routes/index'); // Correct if index.js is in the routes directory
+var usersRouter = require('./routes/users'); // Correct if users.js is in the routes directory
 
 var app = express();
 
@@ -18,13 +20,10 @@ app.set('view engine', 'ejs');
 app.use(expressSession({
   resave: false,
   saveUninitialized: false,
-  secret: "hey hey hey"
-}))
+  secret: process.env.SESSION_SECRET || "default_secret"
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser(usersRouter.serializeUser());
-passport.deserializeUser(usersRouter.deserializeUser());
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -32,8 +31,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter); // Use the updated route
+app.use('/users', usersRouter); // Use the updated route
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,13 +41,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
